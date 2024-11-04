ffc3238f09c82cb71908893a81d9cb30e49cc901
Pablo-Caballero-Maria <pabcabmar3@alum.us.es>
2024-11-04 21:15:56 +0100

    dummy change 2 Closes #3

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '^##ydkswfu0+=ofw0l#$kv^8n)0$i(qd&d&ol#p9!b$8*5%j1+'
-print("dummy change")
+print("dummy change 2")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
